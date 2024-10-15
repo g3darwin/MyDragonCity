7dd6bc43957587e2db4d8c43b4f2b99deb683a9e
Victus <Victus@DESKTOP-QU3I6P6>
2024-10-15 14:00:26 +0200

    Test sobre el funcionamiento de la clase Dragon. Closes #9

--- MyDragonCity/src/mydragoncity/MyDragonCity.java
+++ MyDragonCity/src/mydragoncity/MyDragonCity.java
@@ -306,8 +306,8 @@ public class MyDragonCity {
             StringBuilder sb = new StringBuilder();
             sb.append("Dragon{");
             sb.append("nombre=").append(nombre);
-            sb.append("nivel=").append(nivel);
-            sb.append("precio=").append(precio);
+            sb.append(", nivel=").append(nivel);
+            sb.append(", precio=").append(precio);
             sb.append(", vida=").append(vida);
             sb.append(", danio=").append(danio);
             sb.append(", velocidad=").append(velocidad);
@@ -338,6 +338,7 @@ public class MyDragonCity {
         System.out.println("Tipos de Elementos: " + Arrays.toString(h2.getElementos()));
         System.out.println("Maximo de Dragones: " + h2.getMaxDragones());
         System.out.println("Icono: " + h2.getIcono());
+        System.out.println("Tipo de Habitat: " + h2.getTipo());
         
         h2.setPrecioCompra(150);
         System.out.println("Precio: " + h2.getPrecioCompra());
@@ -352,5 +353,36 @@ public class MyDragonCity {
         System.out.println("Tipos de Elementos: " + Arrays.toString(h2.getElementos()));
         
         System.out.println(h2);
+        
+        
+        Dragon d1 = dragonCity.new Dragon();
+        Dragon d2 = dragonCity.new Dragon("Wallace", 1, 100, 190, 158, 35, 5, 
+                                            "ICONO", TipoDragon.FUEGO);
+        Dragon d3 = dragonCity.new Dragon(d1);
+        
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        
+        System.out.println("Nombre: " + d2.getNombre());
+        System.out.println("Nivel: " + d2.getNivel());
+        System.out.println("Precio: " + d2.getPrecio());
+        System.out.println("Tipo: " + d2.getTipo());
+        System.out.println("Vida: " + d2.getVida());
+        System.out.println("Danio: " + d2.getDanio());
+        System.out.println("Velocidad: " + d2.getVelocidad());
+        System.out.println("Comida: " + d2.getComida());
+        System.out.println("Icono: " + d2.getIcono());
+        
+        String nombre = "Steve";
+        
+        d2.setNombre(nombre);
+        d2.setTipo(TipoDragon.TIERRA);
+        System.out.println(" ");
+        System.out.println("Nombre: " + d2.getNombre());
+        System.out.println("Tipo: " + d2.getTipo());
+        
+        System.out.println(" ");
+        System.out.println(d2);
     }
 }
